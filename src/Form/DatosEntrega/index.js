e387c3871eeee1ddba47666e386fa05d78d5c8a8
Alejandro Reyes <diego_1394_rg@hotmail.com>
2023-05-08 20:46:11 -0600

    Mejorando el codigo y usando useEffect

--- src/Form/DatosEntrega/index.js
+++ src/Form/DatosEntrega/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarInput } from "./validaciones";
 
 const DatosEntrega = ({ updateStep }) => {
+
+  const [address, setAddress] = useState({ value: '', valid: null })
+  const [city, setCity] = useState({ value: '', valid: null })
+  const [province, setProvince] = useState({ value: '', valid: null })
+
+
   return (
     <Box
       component="form"
@@ -23,24 +30,49 @@ const DatosEntrega = ({ updateStep }) => {
         fullWidth
         margin="dense"
         type="text"
+        value={ address.value }
+        onChange={ (input) => {
+          const value = input.target.value
+          const valid = validarInput(value)
+          setAddress({ value, valid })
+        } }
+        error={address.valid === false}
+        helperText={address.valid === false && "Ingresa al menos 4 caracteres"}
       />
+
       <TextField
         label="Ciudad"
         variant="outlined"
         fullWidth
         margin="dense"
         type="text"
+        value={ city.value }
+        onChange={ (input) => {
+          const value = input.target.value
+          const valid = validarInput(value)
+          setCity({ value, valid })
+        } }
+        error={city.valid === false}
+        helperText={city.valid === false && "Ingresa al menos 4 caracteres"}
       />
+
       <TextField
         label="Estado/Provincia"
         variant="outlined"
         fullWidth
         margin="dense"
         type="text"
+        value={ province.value }
+        onChange={ (input) => {
+          const value = input.target.value
+          const valid = validarInput(value)
+          setProvince({ value, valid })
+        } }
+        error={province.valid === false}
+        helperText={province.valid === false && "Ingresa al menos 4 caracteres"}
       />
-      <Button variant="contained" type="submit">
-        Crear cuenta
-      </Button>
+
+      <Button variant="contained" type="submit">Crear cuenta</Button>
     </Box>
   );
 };
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
@@ -6,10 +6,18 @@ import DatosPersonales from "./DatosPersonales";
 import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
 import Stepper from "../Stepper";
+import Step from "./Step";
+
+import { validarEmail, validarPassword } from "./DatosUsuario/validaciones";
 
 const Form = () => {
 
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
+  const [ pasos, setPasos ] = useState({});
+
+  useEffect(() => {
+    
+  }, [step])
 
   //step = 0 ---> <DatosUsuario />
   //step = 1 ---> <DatosPersonales />
@@ -28,6 +36,44 @@ const Form = () => {
     3: <Complete />,
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let newStep = step + 1
+    setPasos(newStep)
+  }
+
+  const handleChange = (element, position, currentStep, validator) => {
+    const value = element.target.value;
+    const valid = validator(value)
+  }
+
+  const stepsFlow = {
+    0: {
+      inputs: [
+        {
+          label: "Correo electrónico",
+          type: "email",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa un correo electrónico válido.",
+          validator: validarEmail,
+        },
+        {
+          label: "Contraseña",
+          type: "Password",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa una contraseña valida, al menos 8 caracteres y maximo 20.",
+          validator: validarPassword,
+        },
+      ],
+      buttonText: "Siguiente",
+      onSubmit
+    },
+  }
+
   return (
     <Box
       sx={{
@@ -42,7 +88,8 @@ const Form = () => {
       </LogoSpace>
       <FormSpace>
         { step < 3 && <Stepper step={step} />}
-        { steps[step] }
+       {/* { steps[step] } */}
+       <Step data = { stepsFlow[step] } step = { step } />
       </FormSpace>
     </Box>
   );
